34b48440045b0f6feeae457b3db6d543b149564c
Schmid Julian <js274@hdm-stuttgart.de>
2015-04-10 18:55:30 +0200

    Erster Prototyp: genaues unter #1 Lichträtzel fehlt.

---
+++ Spiel/Assets/Scripts/FirstPersonController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+public class FirstPersonController : MonoBehaviour {
+
+	public float moveSpeed = 10.0f;
+	public float mouseSensitivety = 2.0f;
+	
+	// Update is called once per frame
+	void Update () {
+
+		float moveVertical = Input.GetAxis ("Vertical")*moveSpeed;
+		float moveHorizontal = Input.GetAxis ("Horizontal")*moveSpeed;
+		//Rotation
+		float rotLeftRight = Input.GetAxis ("Mouse X")*mouseSensitivety;
+
+		transform.Rotate (0,rotLeftRight,0);
+
+		Vector3 speed = new Vector3 (moveHorizontal,0,moveVertical);
+
+		CharacterController cc = GetComponent<CharacterController> ();
+		cc.SimpleMove (speed);
+	}
+}
