22fd4c175fbb56d0ca195a5ce93d7fcfe679a36b
Lukas <Lukasaue@gmx.de>
2015-05-05 15:59:46 +0200

    Ein skript das einen Knopf animiert und die Farben eines Lichtstrahls verändert.

---
+++ Spiel/Assets/Scripts/AnimationButton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationButton : MonoBehaviour {
+
+
+	public Light licht;
+	public Light licht2;
+	public Animation anim;
+
+
+
+	void OnTriggerEnter(Collider other)
+	{
+
+		if (other.gameObject.tag == "Player") {
+
+			anim.Play ("Klick");
+			licht.color = Color.red;
+			licht2.color = Color.green;
+
+		}
+
+	}
+
+}
