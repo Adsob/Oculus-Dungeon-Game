41da261e59af04dd1738cee30683463a32e1c4d0
Lukas <Lukasaue@gmx.de>
2015-05-05 16:10:00 +0200

    FirstPersonController -> Springen möglich Höhlen Mesh neu skaliert

--- Spiel/Assets/Scripts/FirstPersonController.cs
+++ Spiel/Assets/Scripts/FirstPersonController.cs
@@ -1,24 +1,65 @@
 ﻿using UnityEngine;
 using System.Collections;
+
+[RequireComponent (typeof(CharacterController))]
+
 public class FirstPersonController : MonoBehaviour {
+
+	public float movementSpeed = 5.0f;
+	public float mouseSensitivity = 5.0f;
+	public float upDownRange = 60.0f;
+	public float jumpSpeed = 5;
+
+	float verticalRotation = 0;
+
+	float verticalVelocity = 0;
+	CharacterController characterController;
+
+	// Use this for initialization
+	void Start () {
+
 	
-	public float moveSpeed = 10.0f;
-	public float mouseSensitivety = 2.0f;
+		characterController = GetComponent<CharacterController>();	
+
+
+	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-		float moveVertical = Input.GetAxis ("Vertical")*moveSpeed;
-		float moveHorizontal = Input.GetAxis ("Horizontal")*moveSpeed;
+		 
 		//Rotation
-		float rotLeftRight = Input.GetAxis ("Mouse X")*mouseSensitivety;
+
+		float rotLeftRight = Input.GetAxis ("Mouse X") * mouseSensitivity;
+		transform.Rotate (0, rotLeftRight, 0);
+
+		verticalRotation -= Input.GetAxis ("Mouse Y") * mouseSensitivity;
+		verticalRotation = Mathf.Clamp (verticalRotation, -upDownRange, upDownRange);
+		Camera.main.transform.localRotation = Quaternion.Euler (verticalRotation, 0, 0);
+
+
+		//Movement
+
+	
+		float fowardSpeed = Input.GetAxis ("Vertical") * movementSpeed;
+		float sideSpeed = Input.GetAxis ("Horizontal") * movementSpeed;
+
+		//Gravity
+		verticalVelocity += Physics.gravity.y * Time.deltaTime;
+
+		//Jumping
+
+		if (characterController.isGrounded && Input.GetButton ("Jump")) {
 		
-		transform.Rotate (0,rotLeftRight,0);
-		
-		Vector3 speed = new Vector3 (moveHorizontal,0,moveVertical);
+			verticalVelocity = jumpSpeed;
+		}
+
+
+		Vector3 speed = new Vector3 (sideSpeed, verticalVelocity , fowardSpeed);
+
 		speed = transform.rotation * speed; 
-		
-		CharacterController cc = GetComponent<CharacterController> ();
-		cc.SimpleMove (speed);
+
+
+		characterController.Move (speed * Time.deltaTime);
+	
 	}
 }
